4af4be49d96422745484add9ab482dfe76c18f72
diegosoriarios <diegosoriarios@gmail.com>
2019-06-17 15:22:04 -0300

    Search

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Login from './Login'
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const API_KEY = 'Y8npoHmwdkq6xWJMBRcyvF2i4h5Te1tV'
 
@@ -8,34 +10,65 @@ class Home extends Component {
     state = {
         logged: false,
         pesquisa: '',
-        response: []
+        response: [],
+        page: 1,
+        query: ''
     }
 
     fetchApi = query => {
-        console.log(query)
-        axios.get(`https://core.ac.uk/api-v2/search/${query}?page=1&pageSize=10&apiKey=${API_KEY}`)
+        if(this.state.query !== query) {
+            this.setState({
+                response: [],
+                query
+            })
+        }
+        axios.get(`https://core.ac.uk/api-v2/search/${query}?page=${this.state.page}&pageSize=10&apiKey=${API_KEY}`)
         .then(response => {
             return response.data
         })
         .then(response => {
-            response.forEach(obj => {
+            console.log(response.data)
+            response.data.forEach(obj => {
                 this.setState({
-                    response: this.state.response.push(obj)
+                    response: this.state.response.concat(obj._source)
                 })
             })
         })
     }
 
     renderResponse = () => {
-        if(this.state.response !== undefined) {
+        if(this.state.response !== []) {
             return this.state.response.map((obj, i) => {
+                console.log(obj)
                 return (
-                    <li>
-                        <p>{i}</p>
+                    <li className="card" style={{width: '18rem;'}} key={i} >
+                        <div className="card-body">
+                            <div className="row">
+                                <h5 className="card-title col-md-9">{obj.title}</h5>
+                                <FontAwesomeIcon className="col-md-3" size="2x" icon={faHeart} />
+                            </div>
+                            {obj.authors.map(name => (
+                                <h6 className="card-subtitle mb-2 text-muted">{name}</h6>
+                            ))}
+                            
+                            <p className="card-text description">{obj.description.length >= 97 ? obj.description.substring(0, 97) + '...' : obj.description}</p>
+
+                            {obj.urls.map((link, idx) => (
+                                <a href={link} rel="noopener noreferrer" className="card-link" target="_blank">Link {idx + 1}</a>
+                            ))}
+                        </div>
                     </li>
                 );
             })
         }
+    }
+
+    loadMore = () => {
+        this.setState({
+            page: this.state.page + 1
+        }, () => {
+            this.fetchApi(this.state.query)
+        })
     }
 
     render() {
@@ -47,8 +80,9 @@ class Home extends Component {
                     <label htmlFor="pesquisa">Pesquisa</label>
                     <input type="text" id="pesquisa" name="pesquisa" value={pesquisa} onChange={e => this.setState({pesquisa: e.target.value})} />
                     <button onClick={() => this.fetchApi(pesquisa)}>Pesquisa</button>
-                    <ul>
+                    <ul hidden={this.state.response === []}>
                         {this.renderResponse()}
+                        <button onClick={() => this.loadMore()}>+</button>
                     </ul>
                 </div>
             );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
-import configureStore from './store'
+import configureStore from './store/configureStore'
 
 const store = configureStore()
 
